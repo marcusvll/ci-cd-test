664b7139b7bfea59e448b2aaa4523e22633bda4f
Marcus Leite <marcusviniciusll@outlook.com>
2025-06-12 15:44:46 -0300

    Basico Node para demonstração

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// src/index.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Olá do meu aplicativo CI/CD!');
+});
+
+app.get('/api/status', (req, res) => {
+  res.json({ status: 'Rodando', version: '1.0.0' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
