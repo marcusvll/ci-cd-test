3116072a05bdd1330f520f829d0ae6b48372c9c2
Marcus Leite <marcusviniciusll@outlook.com>
2025-06-13 10:11:35 -0300

    Testando deploy 4.0

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Testando deploy 3.0');
+  res.send('Testando deploy 4.0');
 });
 
 app.get('/api/status', (req, res) => {
