6647d389910b80efcf5dbe70163f81d6b09712aa
Marcus Leite <marcusviniciusll@outlook.com>
2025-06-15 20:38:48 -0300

    fix: Atualizar teste unitário para a nova mensagem da API - 2

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -3,10 +3,10 @@ const request = require('supertest');
 const app = require('../index'); // Assumindo que index.js exporta o app
 
 describe('Testando a rota GET /', () => {
-  it('Deve retornar "Altere nas configurações do render o auto deploy"', async () => { // Nome do teste atualizado
+  it('Deve retornar "Alterei nas configurações do render o auto deploy"', async () => { // Nome do teste atualizado
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
     // ATENÇÃO: Altere a string esperada abaixo
-    expect(res.text).toEqual('Altere nas configurações do render o auto deploy');
+    expect(res.text).toEqual('Alterei nas configurações do render o auto deploy');
   });
 });
