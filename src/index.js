044ad2032f5c13fc7a2fa8827495adc933bc1e0a
marcusvll <marcusviniciusll@outlook.com>
2025-06-16 11:34:56 -0300

    Alterando a saída - 3

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   console.log('Requisição GET / recebida.'); // Log de requisição
-  res.send('Alterei nas configurações do render o auto deploy alterando');
+  res.send('Alterei nas configurações do render o auto deploy');
 });
 
 // --- NOVO: Tratamento de erros ---
