74c276d62cb70a5c1379b1494f5909f117040b96
marcusvll <62603467+marcusvll@users.noreply.github.com>
2025-06-16 21:37:58 -0300

    Update app.test.js

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 const app = require('../index'); // Assumindo que index.js exporta o app
 
 describe('Testando a rota GET /', () => {
-  it('Deve retornar "Alterei nas configurações do render o auto deploy"', async () => { // Nome do teste atualizado
+  it('Deve retornar "Alterei nas configurações do render o auto deploy Novamente"', async () => { // Nome do teste atualizado
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
     // ATENÇÃO: Altere a string esperada abaixo
