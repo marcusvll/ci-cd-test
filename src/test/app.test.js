d349ef440af108c2969add1d23b905bc8180e7a5
Marcus Leite <marcusviniciusll@outlook.com>
2025-06-16 22:10:27 -0300

    subindo alterações index e app test js

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -3,10 +3,10 @@ const request = require('supertest');
 const app = require('../index'); // Assumindo que index.js exporta o app
 
 describe('Testando a rota GET /', () => {
-  it('Deve retornar "Alterei nas configurações do render o auto deploy Novamente"', async () => { // Nome do teste atualizado
+  it('Deve retornar "Testando alterações para subir sem erro"', async () => { // Nome do teste atualizado
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
     // ATENÇÃO: Altere a string esperada abaixo
-    expect(res.text).toEqual('Alterei nas configurações do render o auto deploy Novamente');
+    expect(res.text).toEqual('Testando alterações para subir sem erro');
   });
 });
