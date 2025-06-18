38cbb500329d8b16e4154b4cf4221231d28fc001
marcusvll <marcusviniciusll@outlook.com>
2025-06-18 08:32:23 -0300

    Teste sem erro - 2

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -3,10 +3,10 @@ const request = require('supertest');
 const app = require('../index'); // Assumindo que index.js exporta o app
 
 describe('Testando a rota GET /', () => {
-  it('Deve retornar "Testando alterações para subir sem erro"', async () => { // Nome do teste atualizado
+  it('Deve retornar "Testando alterações para subir sem erro - 2"', async () => { // Nome do teste atualizado
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
     // ATENÇÃO: Altere a string esperada abaixo
-    expect(res.text).toEqual('Testando alterações para subir sem erro');
+    expect(res.text).toEqual('Testando alterações para subir sem erro - 2');
   });
 });
