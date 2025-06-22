53f68e6bc511d7b16d7906bbd3e979b3ce249e2a
Marcus Leite <marcusviniciusll@outlook.com>
2025-06-22 17:39:47 -0300

    Ajustando

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,12 +1,9 @@
-// src/test/app.test.js
 const request = require('supertest');
 const app = require('../index');
-
 describe('Testando a rota GET /', () => {
-  it('Deve retornar "Altere nas configurações do render o auto deploy"', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toEqual(200);
-    // Asserção corrigida para o texto atual da sua API
-    expect(res.text).toEqual('Altere nas configurações do render o auto deploy');
-  });
+  it('Deve retornar "Altere nas configurações do render o auto deploy"', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toEqual('Altere nas configurações do render o auto deploy');
+  });
 });
